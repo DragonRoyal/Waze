c2c4953be29df30d073c76a9afd440c1570e20f9
DragonRoyal <79556380+DragonRoyal@users.noreply.github.com>
2024-07-09 17:55:55 -0500

    fix zombie and bullet movement
    fix zombie and bullet movement, also added gun shooting sound with a new level

--- script.js
+++ script.js
@@ -43,6 +43,10 @@ const melody = tune`
 500: D5~500 + B4/500`
 const bullet = "x"
 const zombie = "a"
+const pihit = tune`
+63.29113924050633: B5/63.29113924050633 + A5/63.29113924050633 + G5/63.29113924050633 + C4~63.29113924050633,
+63.29113924050633: A5/63.29113924050633 + C4~63.29113924050633,
+1898.73417721519`
 
 
 
@@ -236,10 +240,13 @@ p........
 .........
 .........`,
   map`
-p.zz
-.bg.
-..z.
-..z.`,
+..........
+..........
+..........
+p.........
+..........
+..........
+..........`,
   map`
 p...
 ...b
@@ -261,7 +268,7 @@ let currentLevel = levels[level];
 setMap(currentLevel);
 
 setSolids([ player, box,zombie]); // other sprites cannot go inside of these sprites
-
+let score = 0
 // allow certain sprites to push certain other sprites
 setPushables({
   [player]: []
@@ -285,6 +292,7 @@ onInput("i", () => {
     pgpos = getFirst(player)
     
     addSprite(pgpos.x + 1,pgpos.y,bullet)
+    playTune(pihit,1)
     
   }
 });
@@ -327,12 +335,67 @@ onInput("j", () => {
 });
 
 
-// these get run after every input
-afterInput(() => {
-  setInterval(function() {
+setInterval(function() {
   // code to be executed repeatedly
   enemyspawn()
-  getFirst(zombie).x -=1;
-}, 3000);
-  bullet.x += 2
-});
+ 
+  getAll(zombie).forEach((zombie) => {
+    zombie.x -= 1
+  });
+ 
+  
+}, 1000);
+
+  
+setInterval(() => {
+  getAll(bullet).forEach((bulletObj) => {
+    getTile(bulletObj.x + 1, bulletObj.y).forEach((sprite) => {
+      if (sprite.type === zombie) {
+        // Zombie detected
+       
+        sprite.remove()
+        bulletObj.remove()
+       score+=1
+        return;
+      }
+    })
+    if (bulletObj.x === 8) {
+      bulletObj.remove();
+    }
+    bulletObj.x += 1
+  }) 
+}, 30)
+
+
+// for score text
+setInterval(() => {
+  if (score != 0){
+    addText(`Score: ${score}`,3,9)
+  }
+  else{
+  }
+}, 30)
+
+setInterval(() => {
+  if (score == 20){
+    level+= 1
+    setMap(levels[level]);
+    score = 0
+  }
+  else{
+  }
+}, 30)
+
+
+
+setInterval(function() {
+  // code to be executed repeatedly
+   getAll(bullet).forEach((bullet) => {
+    bullet.x += 1
+  });
+}, 300);
+
+setInterval(function() {
+  // code to be executed repeatedly
+   
+}, 300);
